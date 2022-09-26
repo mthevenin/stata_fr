dfc0b8cac53a4f9845b55f572af9b33cbcdd9e9d
mthevenin <marc.thevenin@ined.fr>
2022-09-26 14:31:30 +0200

    maj

---
+++ script commentaires.R
@@ -0,0 +1,19 @@
+
+
+
+```{=html}
+<script src="https://giscus.app/client.js"
+data-repo="mthevenin/stata_fr"
+data-repo-id="MDEwOlJlcG9zaXRvcnkxMTAwOTM3NDQ="
+data-category="General"
+data-category-id="DIC_kwDOBo_lsM4CPcVO"
+data-mapping="pathname"
+data-reactions-enabled="1"
+data-emit-metadata="0"
+data-input-position="bottom"
+data-theme="light"
+data-lang="fr"
+crossorigin="anonymous"
+async>
+  </script>
+  ```
